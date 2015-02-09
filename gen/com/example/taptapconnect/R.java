76cfd66dbfeea509301d55b3562124d832268d51
TwnET <a0919958057@gmail.com>
2015-02-10 00:49:42 +0800

    進行所有資源修正

--- gen/com/example/taptapconnect/R.java
+++ gen/com/example/taptapconnect/R.java
@@ -22,9 +22,11 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_search=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
+        public static final int action_search=0x7f080008;
         public static final int button1=0x7f080004;
         public static final int button2=0x7f080005;
         public static final int button3=0x7f080006;
@@ -39,8 +41,10 @@ public final class R {
     }
     public static final class menu {
         public static final int main=0x7f070000;
+        public static final int manu_activity_actions=0x7f070001;
     }
     public static final class string {
+        public static final int action_search=0x7f050005;
         public static final int app_name=0x7f050002;
         public static final int button_green=0x7f050001;
         public static final int button_red=0x7f050000;
